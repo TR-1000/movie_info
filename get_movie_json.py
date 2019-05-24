51b591764ca102786f26808d2fdd472b2f02ee74
TR-1000 <t.l.ross@outlook.com>
2019-05-23 19:08:43 -0500

    updated for a function

--- get_movie_json.py
+++ get_movie_json.py
@@ -1,33 +1,45 @@
-import requests
+# -*- coding: utf-8 -*-
+"""
+Generate movie json file from OMDB API
+Created on Tue May 21 18:31:28 2019
+@author: Miner
+"""
+import os
 import json
-"""get movie info from omdb"""
+import requests
+
+
+def get_movie_json(movie_title):
+    if os.path.isfile(movie_title +"_json.txt") == False:
+        params = {'t':movie_title}
+        response = requests.get('http://www.omdbapi.com/?', params)
+        movie_url = response.url
+        movie_data = response.text
+        movie_json = json.loads(movie_data)
+        with open(movie_title +"_json.txt", "w") as file:
+            for line in movie_data:
+                file.write(line)
+            print("json text saved to file")
+            return movie_json
+    print("json file already exits")
+    file = open(movie_title +"_json.txt", "r")
+    try:
+        file_text = file.read()
+        movie_json = json.loads(file_text)
+        return movie_json
+
+    except:
+        print("returning file didn't work")
+
+    file.close()
+
+top_gun_data = get_movie_json('top gun')
 
 
 
-#search paramaters in form of a dict 't'= movie title
-#d = {'t': 'office space'} 
-input("Enter movie)
-
-d = {'t': 'office space','apikey': 'cb44598d'}
-
-#get request
-response = requests.get('http://www.omdbapi.com/?', params=d) 
-
-#search url
-movie_url = response.url 
-
-#text from website in the form of a string                                     
-movie_data = response_obj.text
-
-#save movie_data string to a file
-with open('office space.txt','w') as file:
-    for line in movie_data:
-        file.write(line)
-
-#coverts movie_data string to json(dictionary)
-movie_json = json.loads(movie_data)
-
-
-        
-        
-    
+#%%
+self.title = title
+        self.params = {'t':movie_title}
+        self.response = requests.get('http://www.omdbapi.com/?', params)
+        self.url = response.url
+        self.movie_text = response.text
